3b94c27e8ac6948ca76040d590b7dd42e3e9975e
perosso <muriloeduborges@gmail.com>
2026-08-20 10:00:10 -0300

    primeira parte

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
+export default function App() {
+  return (
+    
+    <GluestackUIProvider mode="dark">
+      <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+    </GluestackUIProvider>
+  
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
